f9bb46f034db555227c15b7f4733ca0e6ebd3964
Ariful0007 <arifulpub143@gmail.com>
2023-05-03 09:04:41 +0600

    Initial Commit

---
+++ ContainerViewProjects/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContainerViewProjects
+//
+//  Created by sang on 12/10/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
