ff5cfea2e54661f8b44674cbaabc9e60dd89efdb
Ariful0007 <arifulpub143@gmail.com>
2023-05-03 10:30:20 +0600

    vvv

--- ContainerViewProjects/ViewController.swift
+++ ContainerViewProjects/ViewController.swift
@@ -9,22 +9,28 @@ import UIKit
 import Foundation
 
 class ViewController: UIViewController {
-
+    var dataSourceArr = Array<String>()
+              @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let frame1 = CGRect(x:100,y:100,width: 200,height: 200)
-        let myview = UIView(frame: frame1)
-        myview.layer.shadowColor = UIColor.black.cgColor
-        myview.layer.borderColor = UIColor.black.cgColor
-        myview.layer.borderWidth = 12
-        myview.layer.cornerRadius = 99
-        myview.layer.backgroundColor = UIColor.blue.cgColor
-        myview.layer.shadowRadius = 5
-        view.addSubview(myview)
+        /*
+         let frame1 = CGRect(x:100,y:100,width: 200,height: 200)
+         let myview = UIView(frame: frame1)
+         myview.layer.shadowColor = UIColor.black.cgColor
+         myview.layer.borderColor = UIColor.black.cgColor
+         myview.layer.borderWidth = 12
+         myview.layer.cornerRadius = 99
+         myview.layer.backgroundColor = UIColor.blue.cgColor
+         myview.layer.shadowRadius = 5
+         view.addSubview(myview)
+         */
         //second Frame
-        
-        
+        /*
+         tableView.delegate = self
+                tableView.dataSource = self
+                dataSourceArr = ["Python","JavaScript","Java","Swift","GoLang","C#","C++","Scala"]
+         */
     }
    
 
@@ -34,7 +40,22 @@ class ViewController: UIViewController {
 
 
 
-extension ViewController : Any{
-    
+extension ViewController : UITableViewDelegate{
+      
 }
-
+  
+  
+extension ViewController : UITableViewDataSource{
+      
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSourceArr.count
+          
+    }
+      
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = dataSourceArr[indexPath.row]
+        cell.textLabel?.textAlignment = .center
+        return cell
+    }
+}  
