c11c92765dbacc88770ca1171cc175e6588e770a
Ariful0007 <arifulpub143@gmail.com>
2023-05-03 09:42:09 +0600

    v

--- ContainerViewProjects/ViewController.swift
+++ ContainerViewProjects/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let frame1 = CGRect(x:100,y:100,width: 200,height: 200)
+        let myview = UIView(frame: frame1)
+        myview.layer.shadowColor = UIColor.black.cgColor
+        myview.layer.borderColor = UIColor.black.cgColor
+        myview.layer.borderWidth = 12
+        myview.layer.cornerRadius = 99
+        myview.layer.backgroundColor = UIColor.blue.cgColor
+        myview.layer.shadowRadius = 5
+        view.addSubview(myview)
+        //second Frame
+        
+        
     }
+   
 
 
+}
+
+
+
+
+extension ViewController : Any{
+    
 }
 
